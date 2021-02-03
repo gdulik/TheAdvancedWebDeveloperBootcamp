327fd011eb34874353a467b65ac802ee2d1f028f
gdulik <tomas.gdula@gmail.com>
2021-02-03 20:46:28 +0100

    Added map

---
+++ 12-d3/23-map/app.js
@@ -0,0 +1,75 @@
+d3
+	.queue()
+	.defer(
+		d3.json,
+		'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json'
+	)
+	.defer(d3.csv, './country_data.csv', (row) => {
+		return {
+			country: row.country,
+			countryCode: row.countryCode,
+			population: +row.population,
+			medianAge: +row.medianAge,
+			fertilityRate: +row.fertilityRate,
+			populationDensity: +row.population / +row.landArea
+		};
+	})
+	.await((error, mapData, populationData) => {
+		if (error) throw new Error();
+		const geoData = topojson.feature(mapData, mapData.objects.countries)
+			.features;
+		populationData.forEach((row) => {
+			const countries = geoData.filter((d) => d.id === row.countryCode);
+			countries.forEach((country) => (country.properties = row));
+		});
+		const width = 960;
+		const height = 600;
+
+		const projection = d3
+			.geoMercator()
+			.scale(125)
+			.translate([ width / 2, height / 1.4 ]);
+
+		const path = d3.geoPath().projection(projection);
+
+		d3
+			.select('svg')
+			.attr('width', width)
+			.attr('height', height)
+			.selectAll('.country')
+			.data(geoData)
+			.enter()
+			.append('path')
+			.classed('country', true)
+			.attr('d', path);
+
+		const select = d3.select('select');
+
+		select.on('change', (d) => setColor(d3.event.target.value));
+
+		setColor(select.property('value'));
+
+		function setColor(val) {
+			const colorRanges = {
+				population: [ 'white', 'purple' ],
+				populationDensity: [ 'white', 'red' ],
+				medianAge: [ 'white', 'black' ],
+				fertilityRate: [ 'black', 'orange' ]
+			};
+
+			const scale = d3
+				.scaleLinear()
+				.domain([ 0, d3.max(populationData, (d) => d[val]) ])
+				.range(colorRanges[val]);
+
+			d3
+				.selectAll('.country')
+				.transition()
+				.duration(750)
+				.ease(d3.easeBackIn)
+				.attr('fill', (d) => {
+					const data = d.properties[val];
+					return data ? scale(data) : '#ccc';
+				});
+		}
+	});
