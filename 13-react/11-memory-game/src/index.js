f3647a0620c38ff2400eb698eef63888dc973f7e
gdulik <tomas.gdula@gmail.com>
2021-02-10 23:29:46 +0100

    Added memory-game

---
+++ 13-react/11-memory-game/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MemoryGame from './MemoryGame';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<MemoryGame />, document.getElementById('root'));
+
+reportWebVitals();
