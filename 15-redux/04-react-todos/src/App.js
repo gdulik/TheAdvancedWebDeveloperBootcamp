5ee266229f08d1ec589ad564efc83a26b46c0a49
gdulik <tomas.gdula@gmail.com>
2021-02-17 01:00:42 +0100

    Connecting react-todos with backend

--- 15-redux/04-react-todos/src/App.js
+++ 15-redux/04-react-todos/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>Wlcome to the App!</h1>
+				<h1>Welcome to the App!</h1>
 				<p>
 					<Link to="/todos">See my todos!</Link>
 				</p>
--- 15-redux/04-react-todos/src/TodoList.js
+++ 15-redux/04-react-todos/src/TodoList.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Todo from './Todo';
 import { connect } from 'react-redux';
-import { addTodo, removeTodo } from './actionCreators';
+import { addTodo, removeTodo, getTodos } from './actionCreators';
 import { Route } from 'react-router-dom';
 import NewTodoForm from './NewTodoForm';
 
 class TodoList extends Component {
 	constructor(props) {
 		super(props);
-		this.removeTodo = this.removeTodo.bind(this);
 		this.handleAdd = this.handleAdd.bind(this);
+	}
+
+	componentDidMount() {
+		// debugger;
+		this.props.getTodos();
 	}
 
 	handleAdd(val) {
@@ -21,11 +25,12 @@ class TodoList extends Component {
 	}
 
 	render() {
-		let todos = this.props.todos.map((val, index) => (
+		// debugger;
+		let todos = this.props.todos.map((val) => (
 			<Todo
-				removeTodo={this.removeTodo.bind(this, val.id)}
+				removeTodo={this.removeTodo.bind(this, val._id)}
 				task={val.task}
-				key={index}
+				key={val._id}
 			/>
 		));
 		return (
@@ -47,9 +52,12 @@ class TodoList extends Component {
 }
 
 function mapStateToProps(reduxState) {
+	// debugger;
 	return {
 		todos: reduxState.todos
 	};
 }
 
-export default connect(mapStateToProps, { addTodo, removeTodo })(TodoList);
+export default connect(mapStateToProps, { addTodo, removeTodo, getTodos })(
+	TodoList
+);
